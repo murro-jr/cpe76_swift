0fc91619065216a274e873ab4aba81b608d5a3a2
MURRO, JOHN REXES <murrojo_cpe76n@ceac-tcs-imac-5.local>
2018-12-05 16:43:22 +0800

    Initial Commit

---
+++ DietDoctorC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DietDoctorC
+//
+//  Created by MURRO, JOHN REXES  on 05/12/2018.
+//  Copyright © 2018 MURRO, JOHN REXES . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
